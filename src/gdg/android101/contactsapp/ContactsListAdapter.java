29bcfb7c3cb277f6490da33f6c8e0ba0dd429378
Brad Armstrong <brada@code42.com>
2014-01-25 15:35:54 -0600

    Get basic ListActivity and adapter setup

---
+++ src/gdg/android101/contactsapp/ContactsListAdapter.java
@@ -0,0 +1,30 @@
+package gdg.android101.contactsapp;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class ContactsListAdapter extends ArrayAdapter<Contact> {
+
+	public ContactsListAdapter(Context context, int resource, List<Contact> contacts) {
+		super(context, resource, contacts);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View rowView = inflater.inflate(R.layout.contact_list_row, parent, false);
+		TextView textView = (TextView) rowView.findViewById(R.id.contact_name);
+		textView.setText(getItem(position).getFirstName());
+
+		return rowView;
+	}
+
+}
