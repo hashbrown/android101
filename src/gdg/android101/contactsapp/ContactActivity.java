ebfc7880bcd2dfc2ecdb25f6f5c4b4be1c450150
Brad Armstrong <brada@code42.com>
2014-01-25 16:00:05 -0600

    rm crap

--- src/gdg/android101/contactsapp/ContactActivity.java
+++ src/gdg/android101/contactsapp/ContactActivity.java
@@ -3,13 +3,28 @@ package gdg.android101.contactsapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class ContactActivity extends Activity {
+	
+	TextView tvFullName;
+	TextView tvPhoneNbr;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contact_detail);
+		
+		String contactFirstName = getIntent().getStringExtra("firstName");
+		String contactLastName = getIntent().getStringExtra("lastName");
+		String contactPhoneNumber = getIntent().getStringExtra("phone");
+				
+		tvFullName = (TextView) findViewById(R.id.contact_fullname);
+		tvPhoneNbr = (TextView) findViewById(R.id.contact_phone);
+		
+		tvFullName.setText(contactFirstName + " " + contactLastName);
+		tvPhoneNbr.setText(contactPhoneNumber);
+		
 	}
 
 	@Override
