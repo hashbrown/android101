46e0dbad91367da2f45b16e1fbe75fae4691bd8d
Brad Armstrong <brada@code42.com>
2014-01-25 16:14:14 -0600

    Finished complete lesson 3

--- src/gdg/android101/contactsapp/ContactActivity.java
+++ src/gdg/android101/contactsapp/ContactActivity.java
@@ -1,16 +1,20 @@
 package gdg.android101.contactsapp;
 
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ContactActivity extends Activity {
 	
 	TextView tvFullName;
 	TextView tvPhoneNbr;
+	Button btnCall;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,24 @@ public class ContactActivity extends Activity {
 		
 		String contactFirstName = getIntent().getStringExtra("firstName");
 		String contactLastName = getIntent().getStringExtra("lastName");
-		String contactPhoneNumber = getIntent().getStringExtra("phone");
+		final String contactPhoneNumber = getIntent().getStringExtra("phone");
 				
 		tvFullName = (TextView) findViewById(R.id.contact_fullname);
 		tvPhoneNbr = (TextView) findViewById(R.id.contact_phone);
+		btnCall = (Button) findViewById(R.id.call);
 		
 		tvFullName.setText(contactFirstName + " " + contactLastName);
 		tvPhoneNbr.setText(contactPhoneNumber);
+		btnCall.setText("Call " + contactFirstName);
+		
+		btnCall.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				call(contactPhoneNumber);
+				
+			}
+		});
 		
 	}
 
@@ -36,8 +51,8 @@ public class ContactActivity extends Activity {
 		return true;
 	}
 
-	private void call(Contact contact) {
-		Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + contact.getPhone()));
+	private void call(String phoneNbr) {
+		Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNbr));
 		startActivity(dial);
 	}
 }
--- src/gdg/android101/contactsapp/ContactListActivity.java
+++ src/gdg/android101/contactsapp/ContactListActivity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -29,7 +30,12 @@ public class ContactListActivity extends ListActivity {
 	}
 
 	private void showContactDetails(Contact contact) {
-
+		Intent detailsIntent = new Intent(this, ContactActivity.class);
+		detailsIntent.putExtra("firstName", contact.getFirstName());
+		detailsIntent.putExtra("lastName", contact.getLastName());
+		detailsIntent.putExtra("phone", contact.getPhone());
+		
+		startActivity(detailsIntent);
 	}
 
 	@Override
