11b2ab77cec8e1ee12654351707aecf77a9ada69
Jeremy Haberman <jeremy.haberman@code42.com>
2014-01-30 12:39:29 -0600

    Remove lesson 2 work

--- src/gdg/android101/contactsapp/ContactListActivity.java
+++ src/gdg/android101/contactsapp/ContactListActivity.java
@@ -1,38 +1,15 @@
 package gdg.android101.contactsapp;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
 
 public class ContactListActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		ContactsListAdapter adapter = new ContactsListAdapter(this, 0,
-				loadContacts());
-		getListView().setAdapter(adapter);
+		
+		// TODO Create and set adapter on list view
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	private List<Contact> loadContacts() {
-		List<Contact> contacts = new ArrayList<Contact>();
-		try {
-			contacts.addAll(new ContactLoader().loadContacts(this));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return contacts;
-	}
 }
--- src/gdg/android101/contactsapp/ContactsListAdapter.java
+++ src/gdg/android101/contactsapp/ContactsListAdapter.java
@@ -3,11 +3,9 @@ package gdg.android101.contactsapp;
 import java.util.List;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 public class ContactsListAdapter extends ArrayAdapter<Contact> {
 
@@ -18,12 +16,9 @@ public class ContactsListAdapter extends ArrayAdapter<Contact> {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 
-		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.contact_list_row, parent, false);
-		TextView textView = (TextView) rowView.findViewById(R.id.contact_name);
-		textView.setText(getItem(position).getFirstName());
-
-		return rowView;
+		// TODO Implement this
+		
+		return null;
 	}
 
 }
